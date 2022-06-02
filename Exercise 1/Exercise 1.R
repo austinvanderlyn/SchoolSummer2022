419f43cbf07e5e3a87e66afdffd7172bbd759cb8
austinvanderlyn <austinvanderlyn@hotmail.com>
2022-06-01 21:30:30 -0400

    fdsa

--- Exercise 1/Exercise 1.R
+++ Exercise 1/Exercise 1.R
@@ -11,3 +11,18 @@ lm1fit = lm(Expenditure ~ Income, data = spend)
 # slope = 0.06894
 
 # c. intercept from least squares line = -151.26509
+
+# d. find equation for least squares regression line: Y = 0.06894(X) - 151.265
+
+# e. find R^2, or proportion of the variation that can be explained by the equation
+summary(lm1fit)
+
+# f. find sigma^2
+lm1_summary = summary(lm1fit)
+sigma = lm1_summary$sigma
+lm1_var = sigma^2
+lm1_var
+
+
+
+
