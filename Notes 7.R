47cb7108dbdb8c649a149265f56f325f06047cbf
austinvanderlyn <austinvanderlyn@hotmail.com>
2022-07-11 14:59:51 -0400

    fdsa

--- Notes 7.R
+++ Notes 7.R
@@ -24,3 +24,37 @@ ctrl = trainControl(method = "cv",
 #####################
 
 # create a grid for tuning parameters
+nnetGrid = expand.grid(decay = c(0, 0.01, 0.1),
+                       size = c(1, 3, 5, 7),
+                       bag = FALSE)
+
+# it takes time to run
+# the following codes take more than 6,000 seconds to run
+ptm = proc.time()
+set.seed(100)
+nnetTune = train(solTrainXtrans, soltrainY,
+                 method = "avNNet",
+                 tuneGrid = nnetGrid,
+                 trControl = ctrl,
+                 preProcess = c("center", "scale"),
+                 linout = TRUE,
+                 trace = FALSE,
+                 MaxNWts = 13*(ncol(solTrainXtrans) + 1) + 13 + 1,
+                 maxit = 1000,
+                 allowParallel = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
