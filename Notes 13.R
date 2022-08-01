933bba11c33a05b307c20b514520bae648f2d761
austinvanderlyn <austinvanderlyn@hotmail.com>
2022-08-01 13:56:34 -0400

    fdsa

--- Notes 13.R
+++ Notes 13.R
@@ -10,3 +10,38 @@ library(mda)
 library(earth)
 library(e1071)
 library(kernlab)
+data(Smarket)
+
+
+# Data Splitting
+train = which(Smarket$Year<2005)
+Smarket.train = Smarket[train,]
+Smarket.test = Smarket[-train,]
+
+
+# create a control function that will be used across models
+set.seed(123)
+ctrl = trainControl(method = "LGOCV",
+                    summaryFunction = twoClassSummary,
+                    classProbs = TRUE,
+                    savePredictions = TRUE)
+
+
+#########################################
+#### Quadratic Discriminant Analysis ####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
