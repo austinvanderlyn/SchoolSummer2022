7d8a17441a9fa5359b7db0eec67da3cc5d8fbbfb
austinvanderlyn <austinvanderlyn@hotmail.com>
2022-07-31 21:56:53 -0400

    fdsa

---
+++ Notes 13.R
@@ -0,0 +1,12 @@
+# Notes Chapter 13
+
+library(AppliedPredictiveModeling)
+library(caret)
+library(ISLR)
+library(pROC)
+library(MASS)
+library(klaR)
+library(mda)
+library(earth)
+library(e1071)
+library(kernlab)
